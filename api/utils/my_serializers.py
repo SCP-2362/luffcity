a0e0fb80070a430dea56fdcd30a73564153fee31
Chuck <fangming99@outlook.com>
2018-02-28 11:19:32 +0800

    完成课程2/3

---
+++ api/utils/my_serializers.py
@@ -0,0 +1,93 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+
+from .. import models
+
+
+class CourseSerializer(ModelSerializer):
+    level = serializers.SerializerMethodField()
+    detail = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.Course
+        fields = '__all__'
+        depth = 2
+
+    def get_level(self, obj):
+        return obj.get_level_display()
+
+    def get_detail(self, obj):
+        detail_obj = models.CourseDetail.objects.filter(course=obj).first()
+        return CourseDetailSerializer(detail_obj).data
+
+
+class CourseDetailSerializer(ModelSerializer):
+    price = serializers.SerializerMethodField()
+    outline = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.CourseDetail
+        fields = '__all__'
+        # exclude = ["course"]
+        depth = 2
+
+    def get_price(self, obj):
+        return PricePolicySerializer(obj.course.price_policy.all(), many=True).data
+
+    def get_outline(self, obj):
+        return CourseOutlineSerializer(obj.courseoutline_set.all(), many=True).data
+
+
+class CourseChapterSerializer(ModelSerializer):
+    coursesections = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.CourseChapter
+        fields = '__all__'
+        depth = 1
+
+    def get_coursesections(self, obj):
+        return CourseSectionSerializer(obj.coursesections.all(), many=True).data
+
+
+class CourseQuestionSerializer(ModelSerializer):
+    course_question = serializers.CharField(source="coursedetail.oftenaskedquestion")
+
+    class Meta:
+        model = models.Course
+        fields = '__all__'
+        depth = 2
+
+
+class PricePolicySerializer(ModelSerializer):
+    valid_period = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.PricePolicy
+        fields = '__all__'
+        depth = 2
+
+    def get_valid_period(self, obj):
+        return obj.get_valid_period_display()
+
+
+class CourseOutlineSerializer(ModelSerializer):
+    class Meta:
+        model = models.CourseOutline
+        fields = "__all__"
+        depth = 2
+
+
+class CourseSectionSerializer(ModelSerializer):
+    class Meta:
+        model = models.CourseSection
+        fields = "__all__"
+        depth = 1
+
+
+class DegreeCourseSerializer(ModelSerializer):
+    class Meta:
+        model = models.DegreeCourse
+        fields = "__all__"
+        depth = 2
+
--- api/views.py
+++ api/views.py
@@ -1,12 +1,13 @@
-import datetime
 
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
+from api.utils import my_serializers as my_seri
 from . import models
 
 
 class LoginView(APIView):
+    """登录API"""
     def post(self, request, *args, **kwargs):
         # 获取用户名和密码并验证
         username = request.data.get("username", '')
@@ -43,3 +44,84 @@ class LoginView(APIView):
             "data": None,
             "msg": None
         })
+
+
+class CoursesView(APIView):
+    """课程列表API"""
+
+    def get(self, request, *args, **kwargs):
+        res = {
+            "state": 10000,
+            "data": None,
+            "msg": None
+        }
+        wrong_res = {
+            "state": 40000,
+            "data": None,
+            "msg": None
+        }
+        if kwargs.get("pk"):
+            course_obj = models.Course.objects.filter(pk=kwargs.get("pk")).first()
+            if not course_obj:
+                wrong_res["msg"] = "不存在的ID"
+                return JsonResponse(wrong_res)
+            if request.query_params.get("data_type") == "detail":
+                detail_obj = models.CourseDetail.objects.filter(course=course_obj).first()
+
+                if detail_obj:
+                    ser = my_seri.CourseDetailSerializer(instance=detail_obj)
+                else:
+                    wrong_res["msg"] = "暂无课程详细"
+                    return JsonResponse(wrong_res)
+            elif request.query_params.get("data_type") == "chapters":
+                chapter_list = course_obj.coursechapters.all()
+                ser = my_seri.CourseChapterSerializer(instance=chapter_list, many=True)
+            elif request.query_params.get("data_type") == "question":
+                ser = my_seri.CourseQuestionSerializer(instance=course_obj)
+            else:
+                ser = my_seri.CourseSerializer(instance=course_obj)
+        else:
+            course_list = models.Course.objects.all()
+            ser = my_seri.CourseSerializer(instance=course_list, many=True)
+        res["data"] = ser.data
+        return JsonResponse(res)
+
+    def options(self, request, *args, **kwargs):
+        return JsonResponse({
+            "state": 10000,
+            "data": None,
+            "msg": None
+        })
+
+
+class DegreeView(APIView):
+    def get(self, request, *args, **kwargs):
+        res = {
+            "state": 10000,
+            "data": None,
+            "msg": None
+        }
+        wrong_res = {
+            "state": 40000,
+            "data": None,
+            "msg": None
+        }
+        if kwargs.get("pk"):
+            degree_obj = models.DegreeCourse.objects.filter(pk=kwargs.get("pk")).first()
+            if request.query_params.get("data_type") == "detail":
+                pass
+            else:
+                ser = my_seri.DegreeCourseSerializer(degree_obj)
+        else:
+            degree_list = models.DegreeCourse.objects.all()
+            ser = my_seri.DegreeCourseSerializer(instance=degree_list, many=True)
+        res["data"] = ser.data
+        return JsonResponse(res)
+
+    def options(self, request, *args, **kwargs):
+        return JsonResponse({
+            "state": 10000,
+            "data": None,
+            "msg": None
+        })
+
